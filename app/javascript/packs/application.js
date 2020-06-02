e28cdc25213c08d770dd9d6a12b8e5f38aaaea46
Kosuke Ito <keree.it.owl.uk@gmail.com>
2020-06-02 14:31:42 +0900

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,22 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "../src/style.scss";
+import "stylesheets/application.css"
+import "stylesheets/users.css"
+import "stylesheets/sessions.css"
+import "stylesheets/posts.css"
+import "stylesheets/comments.css"
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("jquery")
 // require("@rails/activestorage").start()
 // require("channels")
 
 //= require jquery
 //= require rails-ujs
-
-// import "../src/style.scss";
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
